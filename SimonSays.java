d8e91e78c3cc57977ee44ef067e17f7546afc3ff
smoulding <88937552+smoulding@users.noreply.github.com>
2021-08-22 12:51:00 -0400

    Update SimonSays.java

--- SimonSays.java
+++ SimonSays.java
@@ -4,28 +4,28 @@ public class SimonSays
 {
 	public static void main(String[] args) 
 	   {		   
-		   	  String simonPattern = "";	//Simon's pattern
-		      String userPattern = "";	//User's pattern
-		      int userScore = 0;		//keep score
+		      String simonPattern = "";			//Simon's pattern
+		      String userPattern = "";			//User's pattern
+		      int userScore = 0;			//keep score
 		      int i = 0;				//loop counter
 
 		      userScore = 0;
-		      simonPattern = "RRGBRYYBGY";								//preset Simon pattern
-		      userPattern  = "RRGBBRYBGY";								//preset user's pattern
+		      simonPattern = "RRGBRYYBGY";				//preset Simon pattern
+		      userPattern  = "RRGBBRYBGY";				//preset user's pattern
 
-		      for(i = 0; i < simonPattern.length(); ++i)				//iteration is length of Simon's pattern
+		      for(i = 0; i < simonPattern.length(); ++i)		//iteration is length of Simon's pattern
 		      {
 		    	  if(simonPattern.charAt(i) == userPattern.charAt(i))	//compare characters at index locations-enter of equal
 		    	  {
-		    		  userScore++;										//add 1 to score
+		    		  userScore++;					//add 1 to score
 		    	  }
 		    	  else
 		    	  {
-		    		  break;											//break out of loop once mismatch is found
+		    		  break;					//break out of loop once mismatch is found
 		    	  }
 		      }
 
-		      System.out.println("userScore: " + userScore);			//print score
+		      System.out.println("userScore: " + userScore);		//print score
 
 		      return;
 		   } 	
