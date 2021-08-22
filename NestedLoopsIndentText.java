0e8b9a57a2e3032aee478f3f715a9f145e4a904d
smoulding <88937552+smoulding@users.noreply.github.com>
2021-08-22 11:25:16 -0400

    Add files via upload

---
+++ NestedLoopsIndentText.java
@@ -0,0 +1,23 @@
+package nestedLoopsIndentText;
+
+public class NestedLoopsIndentText 
+{
+	public static void main (String [] args) 
+	{
+	      //initialize variables
+		  int userNum = 3;						//preset to 3
+	      int i = 0;							//loop initializer
+	      int j = 0;							//nested loop initializer
+	      
+	      //indent text
+	      for (i = 0; i <= userNum; ++i)		
+	      {
+	    	  for(j=1; j <= i; ++j)
+	    	  {
+	    		  System.out.print(" ");
+	    	  }	    	  
+	    	  System.out.println(i);	    	  
+	      }
+	      return;
+	}
+}
