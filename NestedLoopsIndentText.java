8c18feee2ba50d07fe960533c90f60767e3906e3
smoulding <88937552+smoulding@users.noreply.github.com>
2021-08-22 11:26:12 -0400

    Update NestedLoopsIndentText.java

--- NestedLoopsIndentText.java
+++ NestedLoopsIndentText.java
@@ -5,7 +5,7 @@ public class NestedLoopsIndentText
 	public static void main (String [] args) 
 	{
 	      //initialize variables
-		  int userNum = 3;						//preset to 3
+	      int userNum = 3;							//preset to 3
 	      int i = 0;							//loop initializer
 	      int j = 0;							//nested loop initializer
 	      
@@ -14,7 +14,7 @@ public class NestedLoopsIndentText
 	      {
 	    	  for(j=1; j <= i; ++j)
 	    	  {
-	    		  System.out.print(" ");
+	    	     System.out.print(" ");
 	    	  }	    	  
 	    	  System.out.println(i);	    	  
 	      }
