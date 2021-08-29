3796108a713d918fe3c809ba258063ee2d93a072
smoulding <88937552+smoulding@users.noreply.github.com>
2021-08-29 19:39:27 -0400

    Update CallDogLicense.java

--- CallDogLicense.java
+++ CallDogLicense.java
@@ -4,11 +4,11 @@ public class CallDogLicense
 {
 	public static void main (String [] args) 
 	{
-	      DogLicense dog1 = new DogLicense();	//new dog license object
+	      DogLicense dog1 = new DogLicense();				//new dog license object
 
-	      dog1.setYear(2014);					//set current year
-	      dog1.createLicenseNum(777);			//set custom id number
-	      System.out.println("Dog license: " + dog1.getLicenseNum());//output license number to terminal
+	      dog1.setYear(2014);						//set current year
+	      dog1.createLicenseNum(777);					//set custom id number
+	      System.out.println("Dog license: " + dog1.getLicenseNum());	//output license number to terminal
 
 	      return;
 	   }
