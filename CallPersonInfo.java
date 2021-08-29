c2b96d23334e50ea63c8129d9e4fd812853ebdf7
smoulding <88937552+smoulding@users.noreply.github.com>
2021-08-29 18:31:54 -0400

    Update CallPersonInfo.java

--- CallPersonInfo.java
+++ CallPersonInfo.java
@@ -4,12 +4,12 @@ public class CallPersonInfo
 {
 	public static void main (String [] args) 
 		{
-	      PersonInfo person1 = new PersonInfo();								//new object
+	      PersonInfo person1 = new PersonInfo();					//new object
 
-	      person1.setNumKids(3);												//set number of kids to 3
+	      person1.setNumKids(3);							//set number of kids to 3
 
-	      System.out.println("Kids: " + person1.getNumKids());					//output to screen
-	      person1.incNumKids();													//invoke incNumKids method
+	      System.out.println("Kids: " + person1.getNumKids());			//output to screen
+	      person1.incNumKids();							//invoke incNumKids method
 	      System.out.println("New baby, kids now: " + person1.getNumKids());	//output to terminal new kids number
 
 	      return;
